c91302ad23e80183f405ea2d2796c7c4269ca12a
arcimiendar <artimendor@yandex.ru>
2021-04-16 00:53:14 +0300

    add simple django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models import Image, ThumbnailSize, ExpirableLink, AccountPlan
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(ThumbnailSize)
+admin.site.register(ExpirableLink)
+admin.site.register(AccountPlan)
